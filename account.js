d5347e3bfe6a564cd403467352cfeb220ac576cc
longfengzhu <longfengzhu@info.easeus.com.cn>
2024-12-27 10:18:15 +0800

    init

---
+++ account.js
@@ -0,0 +1,16 @@
+const accountLists = [
+    {
+      "username": "xxx",
+      "password": "xxx"
+    },
+    {
+      "username": "xxx",
+      "password": "xxx"
+    },
+    {
+      "username": "xxx",
+      "password": "xxx"
+    } // for more users just copy paste the form as many as your accounts
+];
+
+module.exports = accountLists
